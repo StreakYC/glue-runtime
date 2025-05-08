b8210ca0bd9ccfa198039fd8558ce780ad24abb4
Aleem Mawani <aleem@streak.com>
2025-05-08 01:19:23 -0700

    add streak triggers

--- internalTypes.ts
+++ internalTypes.ts
@@ -34,3 +34,4 @@ export type { GithubConfig } from "./integrations/eventSources/github/runtime.ts
 export type { GmailConfig } from "./integrations/eventSources/gmail/runtime.ts";
 export type { WebhookConfig } from "./integrations/eventSources/webhook/runtime.ts";
 export type { CronConfig } from "./integrations/eventSources/cron/runtime.ts";
+export type { StreakConfig } from "./integrations/eventSources/streak/runtime.ts";
