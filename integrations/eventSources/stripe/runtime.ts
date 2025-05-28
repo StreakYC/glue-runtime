6c4777742ad72b522d90e11d1c0d8fd2b4b3ff7f
Aleem Mawani <aleem@streak.com>
2025-05-28 00:16:21 -0700

    stripe runtime support

---
+++ integrations/eventSources/stripe/runtime.ts
@@ -0,0 +1,71 @@
+import type { Stripe as StripeLib } from "npm:stripe";
+import {
+  type CommonTriggerOptions,
+  registerEventListener,
+} from "../../../runtimeSupport.ts";
+
+type StripeEventType = StripeLib.Event.Type;
+
+export type StripeTriggerOptions = CommonTriggerOptions & {
+  accountId?: string;
+};
+
+export interface StripeConfig {
+  events: StripeEventType[];
+  stripeAccountId?: string;
+}
+
+export interface StripeEvent {
+  event: StripeEventType;
+  payload: StripeLib.Event;
+}
+
+export class Stripe {
+  // generic events
+  onEvents(
+    events: StripeEventType[],
+    fn: (event: StripeEvent) => void,
+    options?: StripeTriggerOptions,
+  ): void {
+    const config: StripeConfig = {
+      events,
+      stripeAccountId: options?.accountId,
+    };
+    registerEventListener("stripe", fn, config, options);
+  }
+
+  onCustomerCreated(
+    fn: (event: StripeEvent) => void,
+    options?: StripeTriggerOptions,
+  ): void {
+    this.onEvents(["customer.created"], fn, options);
+  }
+
+  onSubscriptionCreated(
+    fn: (event: StripeEvent) => void,
+    options?: StripeTriggerOptions,
+  ): void {
+    this.onEvents(["customer.subscription.created"], fn, options);
+  }
+
+  onSubscriptionCanceled(
+    fn: (event: StripeEvent) => void,
+    options?: StripeTriggerOptions,
+  ): void {
+    this.onEvents(["customer.subscription.deleted"], fn, options);
+  }
+
+  onPaymentFailed(
+    fn: (event: StripeEvent) => void,
+    options?: StripeTriggerOptions,
+  ): void {
+    this.onEvents(["payment_intent.payment_failed"], fn, options);
+  }
+
+  onPaymentSucceeded(
+    fn: (event: StripeEvent) => void,
+    options?: StripeTriggerOptions,
+  ): void {
+    this.onEvents(["payment_intent.succeeded"], fn, options);
+  }
+}
