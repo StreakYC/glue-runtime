bf5a9ae4757d209bf896653cef3a7247d425ab07
Chris Cowan <agentme49@gmail.com>
2026-02-04 16:15:52 -0800

    update to zod v4

--- common.ts
+++ common.ts
@@ -9,19 +9,17 @@ export interface CommonTriggerOptions {
 }
 
 // ugh I'm sorry about this super explicit type. It needs to be done because
-// otherwise deno's missing-explicit-type lint fails, and it needs to be a
-// ZodObject type instead of ZodType so the `.extend()` method is present.
-export const CommonTriggerOptions: z.ZodObject<
-  {
+// otherwise deno's missing-explicit-type lint fails, it needs to be a ZodObject
+// type instead of ZodType so the `.extend()` method is present, and it's
+// combined with ZodType so the `.parse()` result uses the interface with its
+// tsdoc comments.
+export const CommonTriggerOptions:
+  & z.ZodObject<{
     description: z.ZodOptional<z.ZodString>;
-  },
-  "strip",
-  z.ZodTypeAny,
-  CommonTriggerOptions,
-  CommonTriggerOptions
-> = z.object({
-  description: z.string().optional(),
-});
+  }>
+  & z.ZodType<CommonTriggerOptions, CommonTriggerOptions> = z.object({
+    description: z.string().optional(),
+  });
 
 /**
  * Common options available for all credential fetcher configurations.
@@ -34,17 +32,13 @@ export interface CommonCredentialFetcherOptions {
 // This needs this ugly explicit type because otherwise deno's
 // missing-explicit-type lint fails, and it needs to be a ZodObject type instead
 // of ZodType so the `.extend()` method is present.
-export const CommonCredentialFetcherOptions: z.ZodObject<
-  {
+export const CommonCredentialFetcherOptions:
+  & z.ZodObject<{
     description: z.ZodOptional<z.ZodString>;
-  },
-  "strip",
-  z.ZodTypeAny,
-  CommonCredentialFetcherOptions,
-  CommonCredentialFetcherOptions
-> = z.object({
-  description: z.string().optional(),
-});
+  }>
+  & z.ZodType<CommonCredentialFetcherOptions, CommonCredentialFetcherOptions> = z.object({
+    description: z.string().optional(),
+  });
 
 // these are the same for now but may not be in the future
 
